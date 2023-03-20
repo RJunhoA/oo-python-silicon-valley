8246f86459faf677b79b63fa5857f28a52fa6726
Raymond An <raymond_an@yahoo.com>
2023-03-20 17:32:51 -0400

    completed deliverables for Startup and VentureCapitalist Classes

--- lib/startup.py
+++ lib/startup.py
@@ -2,4 +2,23 @@ from lib.venture_capitalist import *
 from lib.funding_round import *
 
 class Startup:
-    pass
+    all = []
+    def __init__(self, name, founder, domain):
+        self.name = name
+        self.founder = founder
+        self.domain = domain
+        Startup.all.append(self)
+
+    def pivot(self, domain, name):
+        self.domain = domain
+        self.name = name
+
+    @classmethod
+    def find_by_founder(cls, founder):
+        for startup in cls.all:
+            if startup.founder == founder:
+                return startup
+            
+    @classmethod
+    def domains(cls):
+        return {s.domain for s in cls.all}
--- lib/venture_capitalist.py
+++ lib/venture_capitalist.py
@@ -2,4 +2,12 @@ from lib.funding_round import *
 from lib.startup import *
 
 class VentureCapitalist:
-    pass
+    all = []
+    def __init__(self, name, total_worth):
+        self.name = name
+        self.total_worth = total_worth
+        VentureCapitalist.all.append(self)
+
+    @classmethod
+    def tres_commas_club(cls):
+        return [v for v in cls.all if v.total_worth > 1000000]
